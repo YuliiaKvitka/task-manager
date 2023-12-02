79186d0d7b0adbd202bbd446f8665994aefd1189
YuliiaKvitka <modernwebsite21@gmail.com>
2023-12-02 13:30:05 +0200

    Add express with body-parser

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-const sum = (a, b) => a + b;
-console.log(sum(10, 5));
+// const sum = (a, b) => a + b;
+// console.log(sum(10, 5));
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+port = 3000;
+
+// middleware- промежуточное ПО
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(
+    `Server listening on port ${port} and starting at http://localhost:${port}`
+  );
+});
