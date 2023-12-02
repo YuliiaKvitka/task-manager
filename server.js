6dcc7d355cfb28c8d7037736f7decf43f5658a8e
YuliiaKvitka <modernwebsite21@gmail.com>
2023-12-02 19:33:12 +0200

    Created checkAuth and CheckAdmin

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import './config/db.js';
 
 //Routes
 import authRouter from './routes/authRoutes.js';
+import taskRouter from './routes/taskRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -20,6 +21,7 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use('/api', authRouter);
+app.use('/api', taskRouter);
 
 app.listen(port, () => {
     console.log(
